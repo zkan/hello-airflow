dc6d67e5813f7793dbc87a8fd2a1302f86b33162
zkan <kan@prontomarketing.com>
2017-05-06 07:20:20 +0700

    Create DAG for Gu Code <3

---
+++ airflow/dags/gucode.py
@@ -0,0 +1,26 @@
+import datetime
+
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+
+dag = DAG(
+    dag_id='gucode',
+    default_args={'start_date': datetime.datetime(2017, 5, 5)},
+    schedule_interval='*/5 * * * *',
+    catchup=False
+)
+
+t1 = BashOperator(
+    task_id='first_task',
+    bash_command='echo "first task"',
+    dag=dag
+)
+
+t2 = BashOperator(
+    task_id='second_task',
+    bash_command='echo "hello, Gu Code!"',
+    dag=dag
+)
+
+t2.set_upstream(t1)
