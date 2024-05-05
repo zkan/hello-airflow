5de5caf7b2d58c7ec9ce36aa79f9fe5f2c277b4f
zkan <zkan.cs@gmail.com>
2024-05-05 10:56:18 +0700

    Add DAG for backfilling and catching up

---
+++ mnt/dags/example_backfilling.py
@@ -0,0 +1,25 @@
+from airflow.decorators import dag, task
+from airflow.utils import timezone
+
+
+default_args = {
+    "owner": "zkan",
+    "start_date": timezone.datetime(2024, 5, 1),
+}
+@dag(
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=True,
+)
+def example_backfilling():
+
+    @task
+    def get_datestamp(**context) -> str:
+        ds = context["ds"]
+        return ds
+
+
+    get_datestamp()
+
+
+example_backfilling()
